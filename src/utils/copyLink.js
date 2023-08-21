d1f7993110dc8b2eb0d4edc687a37f27201661d7
yuzhixin <1621012142@qq.com>
2023-08-21 11:41:25 +0800

    fix: 修复在移动端无法复制链接

--- src/utils/copyLink.js
+++ src/utils/copyLink.js
@@ -1,12 +1,40 @@
-import { messagePrompt, errorPrompt } from "./messagePrompt"
+import { successPrompt, messagePrompt, errorPrompt } from "./messagePrompt"
+
+// export default async function copyLink() {
+//     const Link = window.location.href
+   
+//     try {
+//         await navigator.clipboard.writeText(Link)
+//         messagePrompt('复制链接成功')
+//     } catch (error) {
+//         errorPrompt('复制链接失败')
+//     }
+// }
 
 export default async function copyLink() {
     const Link = window.location.href
    
+    const textArea = document.createElement('textarea')
+    textArea.value = Link
+    
+    // 将元素设置为不可见
+    textArea.style.opacity = '0'
+    textArea.style.position = 'fixed'
+    textArea.style.pointerEvents = 'none'
+    
+    document.body.appendChild(textArea)
+    textArea.select()
+    
     try {
-        await navigator.clipboard.writeText(Link)
-        messagePrompt('复制链接成功')
+        const successful = document.execCommand('copy')
+        if(successful) {
+            successPrompt('复制链接成功')
+        }else {
+            errorPrompt('复制链接失败')
+        }
     } catch (error) {
         errorPrompt('复制链接失败')
     }
+    
+    document.body.removeChild(textArea)
 }
